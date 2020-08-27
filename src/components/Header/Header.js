a5886e33f0e6bf601be5d843608a5cba7b9ad829
Backsta3 <darkstarfar35@gmail.com>
2020-08-28 00:20:12 +0600

    done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,7 +6,7 @@ import './Header.css';
 const Header = () => {
     return (
         <div className="header">
-       <nav>
+       <nav className="navbar navbar-expand-lg navbar-dark">
            <a href="/shop">Courses</a>
            <a href="/review">Ordered Courses</a>
            <a href="/manage">Inventory</a>
